f7b10ccd7e6d72e8cc9c94f1942ac0e3f278cc50
Srijal Pudasaini <srijalpudasaini89@gmail.com>
2023-09-10 15:21:51 +0545

    Project completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import Header from './Components/Header'
+import Header from './components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { config } from '@fortawesome/fontawesome-svg-core'
